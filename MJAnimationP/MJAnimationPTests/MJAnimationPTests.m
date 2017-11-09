0c2806978699aa9ca4dc9d839b6830b08e5eca22
AlexMJ666 <921711730@qq.com>
2017-11-09 11:01:38 +0800

    start new

---
+++ MJAnimationP/MJAnimationPTests/MJAnimationPTests.m
@@ -0,0 +1,39 @@
+//
+//  MJAnimationPTests.m
+//  MJAnimationPTests
+//
+//  Created by Mac on 2017/11/9.
+//  Copyright © 2017年 Mac. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MJAnimationPTests : XCTestCase
+
+@end
+
+@implementation MJAnimationPTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
